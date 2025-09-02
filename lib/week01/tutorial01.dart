cb6120562522ca555e35a1ed5727db4c4ea1b413
thengelsec <ssuk83158315@gmail.com>
2025-09-02 16:35:40 +0900

    feat(tutorial01): Display 'Hello, World!' in the center

--- lib/week01/tutorial01.dart
+++ lib/week01/tutorial01.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(
     const Center(
-      
+      child: Text(
+        'Hello, World!',
+        textDirection: TextDirection.ltr,
+        
+      )
     )
   )
 }
